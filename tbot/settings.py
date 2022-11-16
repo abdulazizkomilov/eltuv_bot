f5883e2dfe1a1dd730aeb1237f43232e64dffc85
Abdulaziz <abdulazizkomilov2001@gmail.com>
2022-11-16 20:33:50 +0500

    Update settings.py

--- tbot/settings.py
+++ tbot/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from environs import Env
+import os
 
 # Environment variables
 env = Env()
@@ -53,7 +54,7 @@ ROOT_URLCONF = 'tbot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
